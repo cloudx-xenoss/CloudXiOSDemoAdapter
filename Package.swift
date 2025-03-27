d685539320dea15d0697238315210909d31e171f
Bryan Boyko <bryan.boyko@gmail.com>
2025-03-27 14:48:31 -0700

    Update Package.swift to correct config for dependencies

--- Package.swift
+++ Package.swift
@@ -12,12 +12,15 @@ let package = Package(
             targets: ["CloudXDemoAdapter"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/cloudx-xenoss/CloudXCoreiOS.git", from: "1.0.0")
+    ],
     targets: [
         .binaryTarget(
             name: "CloudXDemoAdapter",
             url: "https://github.com/cloudx-xenoss/CloudXiOSDemoAdapter/releases/download/1.0.3/CloudXDemoAdapter.xcframework.zip",
             checksum: "226a782f3e17808ff1598b28d346e3a28f44ecb0738daf8f5dbe1fa069f08b0c"
-        )
+        ),
         .target(
             name: "CloudXDemoAdapterWrapper",
             dependencies: [
@@ -27,4 +30,5 @@ let package = Package(
         )
     ]
 )
+
 
