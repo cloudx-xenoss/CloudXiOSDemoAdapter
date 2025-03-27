758c493baccd437388c68461bc7540bb631e15a5
Bryan Boyko <bryan.boyko@gmail.com>
2025-03-27 14:11:21 -0700

    Remove LLM generated artifact

--- Package.swift
+++ Package.swift
@@ -12,18 +12,11 @@ let package = Package(
             targets: ["CloudXDemoAdapter"]
         ),
     ],
-    dependencies: [
-        .package(url: "https://github.com/cloudx-xenoss/CloudXiOSDemoAdapter.git", from: "1.0.0")
-    ],
     targets: [
         .binaryTarget(
             name: "CloudXDemoAdapter",
             url: "https://github.com/cloudx-xenoss/CloudXiOSDemoAdapter/releases/download/1.0.0/CloudXDemoAdapter.xcframework.zip",
             checksum: "226a782f3e17808ff1598b28d346e3a28f44ecb0738daf8f5dbe1fa069f08b0c"
         ),
-        .target(
-            name: "CloudXDemoAdapterWrapper",
-            dependencies: ["CloudXCore", "CloudXDemoAdapter"]
-        )
     ]
 )
