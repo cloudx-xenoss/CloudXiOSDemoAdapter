10b3989fdcd957c9753b86dbb74537ed8da9bbc8
Bryan Boyko <bryan.boyko@gmail.com>
2025-03-27 14:30:35 -0700

    Update Package.swift to 1.0.1

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CloudXDemoAdapter",
-            url: "https://github.com/cloudx-xenoss/CloudXiOSDemoAdapter/releases/download/1.0.0/CloudXDemoAdapter.xcframework.zip",
+            url: "https://github.com/cloudx-xenoss/CloudXiOSDemoAdapter/releases/download/1.0.1/CloudXDemoAdapter.xcframework.zip",
             checksum: "226a782f3e17808ff1598b28d346e3a28f44ecb0738daf8f5dbe1fa069f08b0c"
         )
     ]
