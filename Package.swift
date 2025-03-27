48f03629a1aee6e2ba243e1a2708b18cc327e85c
Bryan Boyko <bryan.boyko@gmail.com>
2025-03-27 14:20:19 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "CloudXDemoAdapter", 
+    name: "CloudXDemoAdapter",
     platforms: [
         .iOS(.v14)
     ],
@@ -17,6 +17,7 @@ let package = Package(
             name: "CloudXDemoAdapter",
             url: "https://github.com/cloudx-xenoss/CloudXiOSDemoAdapter/releases/download/1.0.0/CloudXDemoAdapter.xcframework.zip",
             checksum: "226a782f3e17808ff1598b28d346e3a28f44ecb0738daf8f5dbe1fa069f08b0c"
-        ),
+        )
     ]
 )
+
